fabe6bbc32c3d68a13178b7474b74fec4a619408
bluue-coder <mf.boden111@gmail.com>
2025-12-04 10:27:52 +0100

    fix: update startup message for ESP32 Cycle Computer update doc

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ SystemState state;
 
 void setup() {
   Serial.begin(Config::BAUD_RATE);
-  Serial.println("ESP32 Cycle Computer - State Machine");
+  Serial.println("ESP32 Cycle Computer Startup...");
   
   // Initialize State Machine
   state.current_state = StateMachine::INIT;
